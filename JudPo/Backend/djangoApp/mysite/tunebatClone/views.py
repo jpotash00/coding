bda59767c050973f42df48f191a7ba53c640df5b
JonathanPotash <jp4226@columbia.edu>
2022-07-13 00:46:12 -0400

    views_algo

--- JudPo/Backend/djangoApp/mysite/tunebatClone/views.py
+++ JudPo/Backend/djangoApp/mysite/tunebatClone/views.py
@@ -42,42 +42,56 @@ def categorySearch(request):
             newstr = value.split()
             value = newstr
     #---->Initial database search to get a list of all the titles and artist as one string
-    mycursor.execute("select concat(title,' ',artist) AS songID FROM songs")
-    rez = mycursor.fetchall()
-    data = list(rez)
-    #---->Initialized empty Data Structures
-    song_dict = {}
-    intArr = np.zeros(len(data)).astype('int')
-    #---->puts everything in dictionary of {word:song_id}
-    for songID in range(len(data)):
-        for word in data[songID]:
-            if ('<' in word or '>' in word):
-                f = filter(str.isalpha, word)
-                s = "".join(f)
-                res_list = re.findall('[A-Z][^A-Z]*',s)
-                data[songID] = res_list
-            else:
-                data[songID] = data[songID][0].split()
+        mycursor.execute("select concat(title,' ',artist) AS songID FROM songs")
+        rez = mycursor.fetchall()
+        data = list(rez)
+        #---->Initialized empty Data Structures
+        song_dict = {}
+        dict_organizer = {}
+        intArr = np.zeros(len(data)).astype('int')
+        #---->puts everything in dictionary of {word:song_id}
+        for songID in range(len(data)):
             for word in data[songID]:
-                if (word in song_dict.keys()):
-                    song_dict[word].add(songID)
+                if ('<' in word or '>' in word):
+                    f = filter(str.isalpha, word)
+                    s = "".join(f)
+                    res_list = re.findall('[A-Z][^A-Z]*',s)
+                    data[songID] = res_list
                 else:
-                    song_dict[word] = set([songID])
-    #---->gets the songID most related to the output from html request
-    for word in value:
-        print(word)
-        x = list(song_dict.get(word))
-        np.add.at(intArr,x,1)
-    max_value = max(intArr)
-    indexTupGreatest = np.where(intArr == max_value) #--> get just one most likely answer
-    songIDIndexGreatest = int(indexTupGreatest[0]+1)
-    strNum = str(songIDIndexGreatest)
-
-     
-    mycursor.execute("select song_id, title, artist, bpm, camelot from songs where song_id = %s", strNum) 
-    res = mycursor.fetchall()
-    for row in res:
-        print(row)
+                    data[songID] = data[songID][0].split()
+                for word in data[songID]:
+                    if (word in song_dict.keys()):
+                        song_dict[word].add(songID)
+                    else:
+                        song_dict[word] = set([songID])
+        #---->gets the a singular songID most related to the output from html request
+        for word in value:
+            x = list(song_dict.get(word))
+            np.add.at(intArr,x,1)
+        max_value = max(intArr)
+        indexTupGreatest = np.where(intArr == max_value) #--> get just one most likely answer
+        songIDIndexGreatest = int(indexTupGreatest[0]+1)
+        strNum = str(songIDIndexGreatest)
+        mycursor.execute("select song_id, title, artist, bpm, camelot from songs where song_id = %s", [strNum]) 
+        # res = mycursor.fetchall()
+        #----> gets list in order of all related songID's
+        songid = -1
+        for rank in intArr:
+            songid+=1
+            if (rank > 0):
+                dict_organizer[songid+1] = rank
+        sort_dict = sorted(dict_organizer.items(),key=lambda x: x[1], reverse=True)
+        newlist = []
+        for i in range(len(sort_dict)):
+            newlist.append(sort_dict[i][0])
+            numStrKey = str(newlist[i])
+            numStrVal = str(sort_dict[i][1])
+            mycursor.execute("UPDATE song_copy SET ranked = %s where song_id = %s", [numStrVal,numStrKey]) #---> idea to create rank system and then delete it so I can get order by rank
+        mycursor.execute("select song_id, title, artist, bpm, camelot from song_copy where ranked > 0 ORDER BY ranked DESC LIMIT 10") #(1,2,12,23,37,125) - for happy now by kygo
+        # songChoices = mycursor.fetchall()
+        # for row in songChoices:
+        #     print(row)
+        mycursor.execute("update song_copy set ranked = NULL where ranked > 0")
     #---------
     # mycursor.execute("select * from songs where songs.title LIKE %s AND songs.artist LIKE %s", [("%" + t + "%"), ("%" + a + "%")])#works with remixes
     # result = mycursor.fetchall() #if more than 1 comes up than choose 1
